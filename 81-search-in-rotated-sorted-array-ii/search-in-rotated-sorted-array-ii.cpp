a44a444ba94ed7bb84f4be36d6d7de10dde8b508
Sanyam7906 <sanyam.humad.07@gmail.com>
2026-07-20 23:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (7.59%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        // last approach fails as
+        // [3,1,2,3,3,3,3]
+        // here a[l]<=a[mid] but the left half is not sorted !! 
+        // this is the only case stopping us
+        // so just add a if codn and shrink the array
+        int l = 0,h = nums.size()-1;
+        while(l<=h)
+        {
+            int mid = (l+h)/2;
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            if(nums[l] == nums[mid] && nums[mid]==nums[h])
+            {
+                l++;
+                h--;
+                continue;
+            }
+            if(nums[l]<=nums[mid])
+            {
+                if(target>=nums[l] && target<=nums[mid])
+                {
+                    h = mid-1;
+                }
+                else
+                {
+                    l = mid+1;
+                }
+            }
+            else
+            {
+                if(target>=nums[mid] && target<=nums[h])
+                {
+                    l = mid+1;
+                }
+                else
+                {
+                    h = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
