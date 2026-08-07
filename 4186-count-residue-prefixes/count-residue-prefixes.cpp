6f57d01340754626d3283c60369209156bcec6e3
Sanyam7906 <sanyam.humad.07@gmail.com>
2026-08-07 14:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (73.36%) - LeetSync

---
+++ 4186-count-residue-prefixes/count-residue-prefixes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int residuePrefixes(string s) {
+        set<char> st;
+        int count = 0;
+        for(int i=0;i<s.size();i++)
+        {
+            st.insert(s[i]);
+            if(st.size() == (i+1)%3)
+            {
+                count++;
+            }
+
+        }
+        return count;
+    }
+};
